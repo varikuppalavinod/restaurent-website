e65b9ddf1b2181e3863c2b67e6c97a5e0b9264c0
varikuppalavinod <varikuppalavinod258@gmail.com>
2024-04-09 14:45:25 +0530

    summary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React,{Fragment} from "react"
 import Header from "./Components/Layout/Header"
+import Meals from "./Components/Meals/Meals"
 const App=()=>{
   return(
     <Fragment>
       <Header></Header>
+      <main>
+        <Meals/>
+      </main>
     </Fragment>
   )
 }
