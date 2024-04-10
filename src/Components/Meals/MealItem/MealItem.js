3e1fa7fa5a840ab6720ce670ae12bedb8350037b
varikuppalavinod <varikuppalavinod258@gmail.com>
2024-04-10 14:19:47 +0530

    addingform

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -1,3 +1,26 @@
+import MealItemForm from './MealItemForm';
+import classes from './MealItem.module.css';
+
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm id={props.id} />
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
+
+/*
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
@@ -16,3 +39,4 @@ const MealItem = (props) => {
 };
 
 export default MealItem;
+*/
