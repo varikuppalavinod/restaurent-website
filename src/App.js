18a5dd4e0727c9025e1e3a8c1437589caac2a02b
varikuppalavinod <varikuppalavinod258@gmail.com>
2024-04-11 10:44:37 +0530

    cart open and close

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
+import { Fragment, useState } from 'react';
+
+import Header from './Components/Layout/Header';
+import Meals from './Components/Meals/Meals';
+import Cart from './Components/Cart/Cart';
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  return (
+    <Fragment>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
+
+/*
 import { Fragment } from 'react';
 
 import Header from './Components/Layout/Header';
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,3 +1,29 @@
+/*
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from"../../asserts/meals.jpg"
+//import mealsimage from '../../asserts/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
+
+
+*/
 import React,{Fragment} from "react";
 import mealsimage from"../../asserts/image.jpg"
 import classes from"./Header.module.css"
@@ -10,7 +36,7 @@ const Header=(props)=>{
             <header className={classes.header}>
                 <h1>React Meals</h1>
                
-               <HeaderCartButton></HeaderCartButton>
+               <HeaderCartButton onClick={props.onShowCart}></HeaderCartButton>
                 
             </header>
             <div  className={classes["main-image"]}>
